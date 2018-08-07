2c849c6f6434fb768da21572fecebd27106ff5c0
Phillip Tennen <pt@datatheorem.com>
2018-08-07 18:59:34 +0200

    draft Game class

--- castle/game.py
+++ castle/game.py
@@ -1,4 +1,13 @@
+from typing import List
+from .board import Board
+from .piece import Piece
 
 
 class Game:
-    pass
+    def __init__(self):
+        self.board = Board()
+        self.pieces: List[Piece] = None
+        self.place_pieces_for_new_game()
+
+    def place_pieces_for_new_game(self):
+        self.board.place_piece(Piece.ROOK, 'a1')
