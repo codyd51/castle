4f593c248fa01e965422d97ba9c9d1ea6723a3d0
Phillip Tennen <pt@datatheorem.com>
2018-08-07 18:39:59 +0200

    add contructor for board's squares

--- castle/board.py
+++ castle/board.py
@@ -1,4 +1,18 @@
+from typing import List
+from .square import Square
 
 
 class Board:
-    pass
+    def __init__(self):
+        self.squares: List[List[Square]] = Board.construct_squares()
+        self.place_startingpieces()
+
+    @classmethod
+    def construct_squares(cls) -> List[List[Square]]:
+        board: List[List[Square]] = []
+        for rank in range(8):
+            for file in range(8):
+                square = Square(rank, file)
+                board[rank][file] = square
+        return board
+
