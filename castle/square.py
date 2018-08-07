5929d4b36d6660d73cb9ff3f40375f925efaf436
Phillip Tennen <pt@datatheorem.com>
2018-08-07 19:05:08 +0200

    convert squares to using int indexes rather than letters

--- castle/square.py
+++ castle/square.py
@@ -1,16 +1,16 @@
-from .piece import Piece
+from castle.piece import Piece
 from typing import List
 
 
 class Square:
     def __init__(self, rank: int, file: int):
-        rank_values = 'abcdefgh'
-        self.rank: int = rank_values[rank]
+
+        self.rank: int = rank
         self.file: int = file
         self.occupant: Piece = None
         self.white_defenders: List[Square] = []
         self.black_defenders: List[Square] = []
 
     def __repr__(self):
-        return f'{self.rank}{self.file}'
-    
+        rank_values = 'abcdefgh'
+        return f'{rank_values[self.rank]}{self.file}'
