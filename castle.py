1e93efeba9a71e74f1c0ad3b129b5ee72a9a77ec
Phillip Tennen <pt@datatheorem.com>
2018-08-08 22:03:09 +0200

    first iteration of game loop/moving pieces

--- castle.py
+++ castle.py
@@ -2,16 +2,11 @@ import castle
 
 
 def main():
-    # g = castle.Game()
-    # g.board.get_moves(g.board.square_from_notation('a1'))
-    b = castle.Board()
-
-    white_rook = castle.Piece(castle.PieceType.ROOK, castle.Color.WHITE)
-    black_pawn = castle.Piece(castle.PieceType.PAWN, castle.Color.BLACK)
-    b.place_piece(white_rook, 'f4')
-    b.place_piece(black_pawn, 'f7')
-    print(b.get_moves(b.square_from_notation('f4')))
-
+    g = castle.Game()
+    g.board.pretty_print()
+    while True:
+        g.play_turn()
+        g.board.pretty_print()
 
 
 if __name__ == '__main__':
