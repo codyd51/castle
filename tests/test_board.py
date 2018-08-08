5838ce91c6bab4d1792c451315fe3f47d1b18c91
Phillip Tennen <pt@datatheorem.com>
2018-08-08 12:27:35 +0200

    add tests for Board.square_from_notation()

---
+++ tests/test_board.py
@@ -0,0 +1,33 @@
+import unittest
+
+from castle import Board, InvalidChessNotationError
+
+
+class BoardTests(unittest.TestCase):
+    def setUp(self):
+        self.board = Board()
+
+    def test_square_from_notation_good(self):
+        # try every valid square on the board
+        for file_idx in range(8):
+            file = chr(ord('a') + file_idx)
+            for rank_idx in range(8):
+                rank = rank_idx + 1
+
+                notation = f'{file}{rank}'
+                square = self.board.square_from_notation(notation)
+
+                self.assertEqual(file_idx, square.file)
+                self.assertEqual(rank_idx, square.rank)
+                self.assertEqual(square.__repr__(), notation)
+
+    def test_square_from_notation_bad(self):
+        # try some invalid notations, and make sure they raise errors
+        with self.assertRaises(InvalidChessNotationError):
+            self.board.square_from_notation('a0')
+        with self.assertRaises(InvalidChessNotationError):
+            self.board.square_from_notation('00')
+        with self.assertRaises(InvalidChessNotationError):
+            self.board.square_from_notation('aa')
+        with self.assertRaises(InvalidChessNotationError):
+            self.board.square_from_notation('===')
