e5996c8d8be5adc77a93623aa9ff76da7bdefb8c
Phillip Tennen <pt@datatheorem.com>
2018-08-10 00:52:33 +0200

    update Game tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -12,48 +12,48 @@ class GameTests(unittest.TestCase):
 
     def test_simple(self):
         g = castle.Game()
-        square_from, square_to = castle.MoveParser.parse_move(g.board, castle.Color.WHITE, 'e4')
-        self.assertEqual('e2', square_from.notation())
-        self.assertEqual('e4', square_to.notation())
+        move = castle.MoveParser.parse_move(g.board, castle.Color.WHITE, 'e4')
+        self.assertEqual('e2', move.from_square.notation())
+        self.assertEqual('e4', move.to_square.notation())
 
         # 1. e4 e5 2. Bd3
-        g.apply_move('e4')
-        square_from, square_to = castle.MoveParser.parse_move(g.board, castle.Color.WHITE, 'Bd3')
-        self.assertEqual('f1', square_from.notation())
-        self.assertEqual('d3', square_to.notation())
+        g.apply_notation('e4')
+        move = castle.MoveParser.parse_move(g.board, castle.Color.WHITE, 'Bd3')
+        self.assertEqual('f1', move.from_square.notation())
+        self.assertEqual('d3', move.to_square.notation())
 
     def test_bishop_capture(self):
         g = castle.Game()
-        g.apply_move('e4')
-        g.apply_move('e5')
-        g.apply_move('d4')
-        g.apply_move('a6')
-        g.apply_move('Bxa6')
+        g.apply_notation('e4')
+        g.apply_notation('e5')
+        g.apply_notation('d4')
+        g.apply_notation('a6')
+        g.apply_notation('Bxa6')
         self.check_contains_piece(g, 'a6', PieceType.BISHOP, Color.WHITE)
 
     def test_pawn_capture(self):
         g = castle.Game()
-        g.apply_move('e4')
-        g.apply_move('f5')
+        g.apply_notation('e4')
+        g.apply_notation('f5')
         # TODO(PT): once MoveParser returns a Move object, this should verify a capture was indicated
-        g.apply_move('exf5')
+        g.apply_notation('exf5')
         self.check_contains_piece(g, 'f5', PieceType.PAWN, Color.WHITE)
 
     def test_knight_movement(self):
         g = castle.Game()
-        g.apply_move('e4')
-        g.apply_move('e5')
-        g.apply_move('Ne2')
+        g.apply_notation('e4')
+        g.apply_notation('e5')
+        g.apply_notation('Ne2')
         self.check_contains_piece(g, 'e2', PieceType.KNIGHT, Color.WHITE)
 
     def test_ambiguous_knight_movement(self):
         g = castle.Game()
-        g.apply_move('e4')
-        g.apply_move('e5')
-        g.apply_move('Ne2')
-        g.apply_move('f6')
+        g.apply_notation('e4')
+        g.apply_notation('e5')
+        g.apply_notation('Ne2')
+        g.apply_notation('f6')
         with self.assertRaises(InvalidChessNotationError):
-            g.apply_move('Nc3')
+            g.apply_notation('Nc3')
 
-        g.apply_move('Nec3')
+        g.apply_notation('Nec3')
         self.check_contains_piece(g, 'c3', PieceType.KNIGHT, Color.WHITE)
