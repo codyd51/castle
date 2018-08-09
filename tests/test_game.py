b69348707ea7858aa09690af53b34ae62a2bf77e
Phillip Tennen <pt@datatheorem.com>
2018-08-09 19:14:07 +0200

    add another integration test

--- tests/test_game.py
+++ tests/test_game.py
@@ -16,7 +16,18 @@ class GameTests(unittest.TestCase):
         self.assertEqual('e4', square_to.notation())
 
         # 1. e4 e5 2. Bd3
-        g.board.move_piece_to_square(g.board.square_from_notation('e2'), g.board.square_from_notation('e4'))
+        g.apply_move('e4')
         square_from, square_to = castle.MoveParser.parse_move(g.board, castle.Color.WHITE, 'Bd3')
         self.assertEqual('f1', square_from.notation())
         self.assertEqual('d3', square_to.notation())
+
+    def test_bishop_capture(self):
+        g = castle.Game()
+        g.apply_move('e4')
+        g.apply_move('e5')
+        g.apply_move('d4')
+        g.apply_move('a6')
+        g.apply_move('Bxa6')
+        bishop_square = g.board.square_from_notation('a6')
+        self.assertTrue(bishop_square.occupant.type == PieceType.BISHOP)
+        self.assertTrue(bishop_square.occupant.color == Color.WHITE)
