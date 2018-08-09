fe1e95685257f9a419d1bdbb6756d4f3f7f0d2b9
Phillip Tennen <pt@datatheorem.com>
2018-08-09 17:38:23 +0200

    add Square.index_to_file()

--- castle/square.py
+++ castle/square.py
@@ -11,11 +11,15 @@ class Square:
         self.black_defenders: List[Square] = []
 
     @staticmethod
-    def file_to_index(file: str):
+    def file_to_index(file: str) -> int:
         # TODO(PT): replace usage throughout project with this
         return ord(file) - ord('a')
 
-    def __repr__(self):
+    @staticmethod
+    def index_to_file(idx: int) -> str:
         file_values = 'abcdefgh'
+        return file_values[idx]
+
+    def __repr__(self):
         # ranks are 1-indexed
-        return f'{file_values[self.file]}{self.rank + 1}'
+        return f'{Square.index_to_file(self.file)}{self.rank + 1}'
